73505677d0252c9c79e95de4991201cb50903dd4
Haoyu Sheng <hshe0870@uni.sydney.edu.au>
2021-06-18 09:01:46 +0200

    add rmprop

--- run.sh
+++ run.sh
@@ -26,6 +26,38 @@ python main.py --opt sgd --lr 0.4 --bs 512 --warmup_epochs 10 --psuedo 32
 python main.py --opt sgd --lr 0.8 --bs 1024 --warmup_epochs 10 --psuedo 32
 python main.py --opt sgd --lr 1.6 --bs 2048 --warmup_epochs 10 --psuedo 32
 
+# RMSProp
+# tuning learning rate for RMSProp
+python main.py --opt rmsprop --lr 0.1 --bs 128 --lr_decay 0.1 
+python main.py --opt rmsprop --lr 0.01 --bs 128 --lr_decay 0.1 
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.1 
+python main.py --opt rmsprop --lr 0.0001 --bs 128 --lr_decay 0.1 
+
+# tuning weight decay for RMSProp
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.1 
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.5 
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.05 
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.01 
+
+
+# tuning batch size for RMSProp
+python main.py --opt rmsprop --lr 0.001 --bs 32 --lr_decay 0.05 
+python main.py --opt rmsprop --lr 0.001 --bs 64  --lr_decay 0.05  
+python main.py --opt rmsprop --lr 0.001 --bs 128 --lr_decay 0.05 
+python main.py --opt rmsprop --lr 0.001 --bs 256 --lr_decay 0.05  
+python main.py --opt rmsprop --lr 0.001 --bs 512 --lr_decay 0.05 
+
+
+# simulation of multi-worker training
+python main.py --opt rmsprop --lr 0.00025 --bs 32 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.0005 --bs 64 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.001 --bs 128 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.002 --bs 256 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.004 --bs 512 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.008 --bs 1024 --warmup_epochs 10 --psuedo 128
+python main.py --opt rmsprop --lr 0.016 --bs 2048 --warmup_epochs 10 --psuedo 128
+
+
 
 
 
